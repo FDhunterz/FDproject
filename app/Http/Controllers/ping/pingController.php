ffd0db4fad5cf5b8ad486bdc7687c61bd1f9337c
faizal <faizaltriswanto@gmail.com>
2019-09-03 17:40:18 +0700

    y

---
+++ app/Http/Controllers/ping/pingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\ping;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class pingController extends Controller
+{
+    public function index()
+    {
+    	return view('ping_parameter');
+    }
+}
